3697073cd28fceb6bca123713441f2b362a08d62
joseparhusip <joseparhusip7@gmail.com>
2025-09-08 09:31:16 +0700

    Update Vercel config for Laravel + Vite

--- api/index.php
+++ api/index.php
@@ -1,3 +1,12 @@
 <?php
 
+// Set correct server variables for Laravel
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['REQUEST_URI'] = $_SERVER['REQUEST_URI'] ?? '/';
+
+// Change working directory to Laravel root
+chdir(__DIR__ . '/..');
+
+// Include Laravel's main entry point
 require __DIR__ . '/../public/index.php';
